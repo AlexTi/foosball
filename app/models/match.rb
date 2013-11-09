2acd73787104e436dcd5411bc496a5c05cca74be
beanieboi <beanie@benle.de>
2013-11-09 17:27:40 +0100

    add Match#winner method

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,12 @@
 class Match < ActiveRecord::Base
   belongs_to :home_team, class_name: 'Team'
   belongs_to :away_team, class_name: 'Team'
+
+  def winner
+    if self.home_team_score > self.away_team_score
+      self.home_team
+    else
+      self.away_team
+    end
+  end
 end
