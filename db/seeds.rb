844fa787252a773994c2617389849cfa8647b2b2
beanieboi <beanie@benle.de>
2013-11-09 17:27:27 +0100

    add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+alex = Player.create! name: 'AlexTi'
+ben = Player.create! name: 'beanieboi'
+manni = Player.create! name: 'manni'
+clemens = Player.create! name: 'clemens'
+
+team1 = Team.create forward_player: alex, defense_player: ben
+team2 = Team.create forward_player: clemens, defense_player: manni
+
+match1 = Match.create home_team: team1, away_team: team2,
+                      home_team_score: 10, away_team_score: 0
+
+match2 = Match.create home_team: team1, away_team: team2,
+                      home_team_score: 8, away_team_score: 0
+
+match3 = Match.create home_team: team1, away_team: team2,
+                      home_team_score: 9, away_team_score: 10
