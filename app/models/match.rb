9966fae97b125c5be8959b221277c52e33e1971a
beanieboi <beanie@benle.de>
2013-11-09 18:07:58 +0100

    add Match.finished and Match.unfinished scopes

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,6 +2,9 @@ class Match < ActiveRecord::Base
   belongs_to :home_team, class_name: 'Team'
   belongs_to :away_team, class_name: 'Team'
 
+  scope :finished,    -> { where.not(home_team_score: nil).where.not(away_team_score: nil) }
+  scope :unfinished,  -> { where(home_team_score: nil).where(away_team_score: nil) }
+
   def winner
     if self.home_team_score > self.away_team_score
       self.home_team
