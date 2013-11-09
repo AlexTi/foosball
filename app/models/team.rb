d5538edd835052d434f181ed0b93cba30c00e79a
beanieboi <beanie@benle.de>
2013-11-09 18:01:14 +0100

    add Team.from_players

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,6 +5,10 @@ class Team < ActiveRecord::Base
   has_many :home_matches, class_name: 'Match', foreign_key: :home_team_id
   has_many :away_matches, class_name: 'Match', foreign_key: :away_team_id
 
+  def self.from_players forward_player: forward_player, defense_player: defense_player
+    Team.where(forward_player_id: forward_player.id).where(defense_player_id: defense_player.id).first_or_create
+  end
+
   def matches
     (home_matches + away_matches).uniq
   end
