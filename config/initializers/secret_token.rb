c50b73a9b6d734d8a32e62e714477f84c2615b33
beanieboi <beanie@benle.de>
2013-11-12 20:58:11 +0100

    use a different secret token on heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Foosball::Application.config.secret_key_base = '9c94d1594ec4a3fc628bb9db51d16caf9aa2407152b145acc28020be8c2d7cb997e61144b3abcffd50d0f1bc9913a3cb9150901cd48dffd90f27c8a384cda35a'
+Foosball::Application.config.secret_key_base = ENV['secret_key_base'] || '9c94d1594ec4a3fc628bb9db51d16caf9aa2407152b145acc28020be8c2d7cb997e61144b3abcffd50d0f1bc9913a3cb9150901cd48dffd90f27c8a384cda35a'
