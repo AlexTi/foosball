74f47d03436a1c5dcce1590d0b2a29970dc8158d
beanieboi <beanie@benle.de>
2013-11-24 15:58:20 +0100

    move CombinedTeam into it's own class

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -43,29 +43,3 @@ class Ranking
     end
   end
 end
-
-class CombinedTeam
-  def initialize teams_to_combine
-    @teams_to_combine = teams_to_combine
-  end
-
-  def name
-    @teams_to_combine.first.name
-  end
-
-  def matches
-    @teams_to_combine.collect(&:matches).flatten
-  end
-
-  def points
-    @teams_to_combine.map(&:points).reduce(:+)
-  end
-
-  def goals
-    @teams_to_combine.map(&:goals).reduce(:+)
-  end
-
-  def goals_conceded
-    @teams_to_combine.map(&:goals_conceded).reduce(:+)
-  end
-end
